efdd69459e9a51b652cc9d72c238dddbce89dfa9
George <giwrgosteg@gmail.com>
2024-06-26 00:10:15 +0300

    Made the opacity feature!

--- code.js
+++ code.js
@@ -3,8 +3,47 @@ let elements = document.querySelectorAll("div.sketch");
 let containers = document.querySelectorAll("div.container");
 
 function colorChange() {
-    this.style.backgroundColor = "black";
-    this.style.opacity = 0.1;
+    if (this.id == "1") {
+        this.style.backgroundColor = "black";
+        this.style.opacity = 0.1;
+        this.id = "0.1";
+    }
+    else if (this.id == "0.1") {
+        this.style.opacity = 0.2;
+        this.id = "0.2";
+    }
+    else if (this.id == "0.2") {
+        this.style.opacity = 0.3;
+        this.id = "0.3";
+    }
+    else if (this.id == "0.3") {
+        this.style.opacity = 0.4;
+        this.id = "0.4";
+    }
+    else if (this.id == "0.4") {
+        this.style.opacity = 0.5;
+        this.id = "0.5";
+    }
+    else if (this.id == "0.5") {
+        this.style.opacity = 0.6;
+        this.id = "0.6";
+    }
+    else if (this.id == "0.6") {
+        this.style.opacity = 0.7;
+        this.id = "0.7";
+    }
+    else if (this.id == "0.7") {
+        this.style.opacity = 0.8;
+        this.id = "0.8";
+    }
+    else if (this.id == "0.8") {
+        this.style.opacity = 0.9;
+        this.id = "0.9";
+    }
+    else if (this.id == "0.9") {
+        this.style.opacity = 1;
+        this.id = "-1";
+    }
 }
 
 function colorReset(element) {
@@ -29,12 +68,11 @@ function rescaleSize() {
         }
         for (let i = 0; i < size; i++) {
             const bigDivo = document.createElement("div");
-            bigDivo.id = "bigBlock";
             bigDivo.classList.add("container");
             for (let j = 0; j < size; j++) {
                 const divo = document.createElement("div");
         
-                divo.id = "block";
+                divo.id = "1";
                 divo.classList.add("sketch");
                 bigDivo.appendChild(divo);
             }
@@ -53,10 +91,7 @@ function rescaleSize() {
 }
 
 for (let i = 0; i < elements.length; i++){
-    elements[i].addEventListener("mouseover", function() { 
-        this.style.backgroundColor = "black";
-        this.style.opacity += 0.1;
-    })
+    elements[i].addEventListener("mouseover", colorChange);
 }
 
 document.getElementById("reset").onclick = function() {
